e14c1cf08ff7777547656d94937a25cbfbe68f1f
RobertWhyte <robert.whyte@zeroprojects.ca>
2019-08-02 16:21:53 -0400

    Cleaned up documentation.  Made GET, PUT, POST, DELETE use options constructor function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 const {version} = require('../package.json');
 const urlJoin = require('url-join');
 
-// class WsInitializationError extends Error {}
-
-// function requirePresence(options, property) {
-//   if (!options || !options[property]) {
-//     throw new WsInitializationError(`Option ${property} is mandatory`);
-//   }
-// }
-
 const SDK_READY = 'SDK_READY';
 
 export class WealthscopeSdk {
@@ -136,12 +128,14 @@ export class WealthscopeSdk {
 }
 
 export class WealthscopeApiClient {
+  /**
+   * Constructor for the Wealthscope Client API.
+   * Stores the authorization token for the current session, and the base URl.
+   * @param {object} opts 
+   */
   constructor(opts) {
-    // This is the login token for Wealthscope,
-    // call login to get a valid token, callll logout to reset it to null
     this.token = null;
 
-    // Default options, currently just the base URL for Wealthscope
     this.opts = Object.assign(
         {
           wealthscopeUrl: 'https://api.staging-bus.wealthscope.ca/v1',
@@ -150,14 +144,20 @@ export class WealthscopeApiClient {
     );
   }
 
-  // This get's the current version of Wealthscope
+  /**
+   * Function that returns the current version from the package.json.
+   * @returns {string}
+   */
   static version() {
     return version;
   }
 
-  // This retrieves a valid token and sets this.token to be the valid token
+  /**
+   * This retrieves a valid token and sets this.token to be the valid token.
+   * @param {string} jwtData 
+   * @returns {Promise}
+   */ 
   login(jwtData) {
-    // const loginOptions = this._getFetchOptions('POST', { token: jwtData });
     return fetch(this.opts.wealthscopeUrl + '/auth/authenticate/', {
       method: 'POST',
       headers: {
@@ -175,98 +175,106 @@ export class WealthscopeApiClient {
         });
   }
 
-  // This resets the token to null
+  /**
+   * This resets the token to null.
+   */
   logout() {
     this.token = null;
   }
 
-  // fetch call returning a Promise, requires a URL rout and a vailid token
+  /**
+   * Fetch call returning a Promise, requires a URL rout and a vailid token.
+   * @param {string} url 
+   * @returns {Promise}
+   */
   get(url) {
-    const options = {
-      method: 'GET',
-      //mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + this.token,
-      },
-    };
-
-    return fetch(this._constructUrl(url), options)
+    const postOptions = this._getFetchOptions('GET');
+    return fetch(this._constructUrl(url), postOptions)
       .then(response => response.json())
       .catch(err => console.log(err));
   }
 
-  // Fetch call returning a Promise, requires a URL rout, a body and a valid
-  // token
-  // NOTE: Body needs to be a JSON object here, it gets stringified when it is
-  // added to the options object for the fetch
+  /**
+   * Fetch call returning a Promise, requires a URL rout, a body and a valid
+   * token.
+   * NOTE: Body needs to be a JSON object here, it gets stringified when it is
+   * added to the options object for the fetch.
+   * @param {string} url 
+   * @param {object} body 
+   * @returns {Promise}
+   */
   put(url, body) {
-    const options = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + this.token,
-      },
-      body: JSON.stringify(body),
-    };
-
-    return fetch(this._constructUrl(url), options)
+    const postOptions = this._getFetchOptions('PUT', body);
+    return fetch(this._constructUrl(url), postOptions)
       .then(response => response.json())
       .catch(err => console.log(err));
   }
 
+  /**
+   * Fetch call returning a Promise, requires a URL rout, a body and a valid
+   * token.
+   * NOTE: Body needs to be a JSON object here, it gets stringified when it is
+   * added to the options object for the fetch.
+   * @param {string} url 
+   * @param {object} body 
+   * @returns {Promise}
+   */
   post(url, body) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + this.token,
-      },
-      body: JSON.stringify(body),
-    };
-
-    return fetch(this._constructUrl(url), options)
+    const postOptions = this._getFetchOptions('POST', body);
+    return fetch(this._constructUrl(url), postOptions)
       .then(response => response.json())
       .catch(err => console.log(err));
   }
 
+  /**
+   * Fetch call returning a Promise, requires a URL rout, a body and a valid
+   * token.
+   * NOTE: Body needs to be a JSON object here, it gets stringified when it is
+   * added to the options object for the fetch.
+   * @param {string} url 
+   * @param {object} body 
+   * @returns {Promise}
+   */
   del(url, body) {
-    const options = {
-      method: 'DELETE',
-      //mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + this.token,
-      },
-      body: JSON.stringify(body),
-    };
-
-    return fetch(this._constructUrl(url), options)
+    const postOptions = this._getFetchOptions('DELETE', body);
+    return fetch(this._constructUrl(url), postOptions)
       .then(response => response.json())
       .catch(err => console.log(err));
   }
 
-  // this function generates a URL
+  /**
+   * Generates a URL, and ensures a trailing '/'
+   * @param {string} url
+   * @returns {string} 
+   * @private
+   */
   _constructUrl(url) {
     // trailing slash is important in order for `fetch` to work
     return urlJoin(this.opts.wealthscopeUrl, url) + '/'
   }
 
-  // This function constructs the options object that will be used by the
-  // various fetch calls below
+  /**
+   * This function constructs the options object that will be used by the
+   * various fetch calls below.
+   * NOTE: method must be in ALL CAPS, body must be a json object
+   * @param {string} method 
+   * @param {object} body 
+   * @returns {object}
+   * @private
+   */
   _getFetchOptions(method, body) {
-    // An empty options object
+    // An options object with empty headers
     const fetchOption = {
       headers: {}
     };
 
     // The Method portion of the object
     fetchOption.method = method;
-    //fetchOption.mode = 'no-cors';
+
     // A check to make sure token is present before adding it to the options
     // return an error if token isn't present
     if (this.token != null) {
-      fetchOption.headers.Authentication = 'JWT ' + this.token;
+      fetchOption.headers.Authorization = 'JWT ' + this.token;
     } else {
       throw new Error('You must be logged in to perform this action.');
     }
@@ -284,6 +292,5 @@ export class WealthscopeApiClient {
   }
 
 }
-
 
 window.API = WealthscopeApiClient;
