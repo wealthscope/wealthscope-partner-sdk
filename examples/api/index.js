794b3301273210429e61a1cc65d137196a50b6e1
Monarch Wadia <monarch@zeroprojects.ca>
2019-08-06 08:53:08 -0400

    Clarifications to api example

--- examples/api/index.js
+++ examples/api/index.js
@@ -16,9 +16,14 @@ async function run() {
   const api = new WealthscopeApiClient(options);
   const loginPayload = constructJwtPayload();
   
+  console.log('Performing login.');
   await api.login(loginPayload);
-  console.log(api);
+  console.log('RESULT: login, token field should be populated', api);
 
+  // Wait 2 seconds before next API call
+  await waitMs(2000);
+
+  console.log('Performing portfolio/analytics call.');
   const results = await api.post('portfolio/analytics', {
     symbols: "KXI,L:CA,MDLZ,QQQ,RQH:CA,WN:CA,XCV:CA,XDV:CA,XIN:CA,XIU:CA,XMW:CA,XSP:CA",
     weights: "0.0639,0.0942,0.0932,0.1409,0.0959,0.0196,0.0398,0.0761,0.0798,0.0295,0.1934,0.0737",
@@ -27,7 +32,10 @@ async function run() {
     init_date: "2007-08-01",
     end_date: "2018-11-30"
   });
-  console.log(await results.json());
+  console.log('RESULT: portfolio/analytics', await results.json());
+
+  // Wait 2 seconds before next API call
+  await waitMs(2000);
 }
 
 run();
@@ -46,4 +54,15 @@ function constructJwtPayload() {
   }
 
   return jwt.sign(payload, privateKey, {algorithm: 'RS512'});
+}
+
+/**
+ * Sleep function to throttle calls to the API
+ * @param {Promise} timeMs 
+ */
+function waitMs(timeMs) {
+  console.log('Sleeping for ' + timeMs + ' milliseconds.');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), timeMs);
+  });
 }
